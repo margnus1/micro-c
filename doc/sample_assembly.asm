2d040ac9c8e99faab87d6f27d78a50f0a0968818
Magnus Lång <margnus1@telia.com>
2014-03-04 14:15:11 +0100

    Fixed stack offset off-by-one bug in sample_assembly

--- doc/sample_assembly.asm
+++ doc/sample_assembly.asm
@@ -8,9 +8,10 @@
 # }
 .globl main
 main:
-	addiu	$sp, $sp, -16
-	sw	$fp, 16($sp)
-	sw	$ra, 12($sp)
+	addiu	$sp, $sp, -16 # We allocate stack slots 12($sp), 8($sp), 4($sp) and
+	     	              # 0($sp)
+	sw	$fp, 12($sp)
+	sw	$ra, 8($sp)
 	move 	$fp, $sp
 	
 	# We only need 4 bytes of stack space for the call, but since the
@@ -19,8 +20,6 @@ main:
 	addiu	$sp, $sp, -8
 	la	$a0, main_buffer
 	jal	getstring
-	# # Store return value in a stack temporary
-	# sw	$v0, -8($fp)
 	addiu	$sp, $sp, 8
 	
 	addiu	$sp, $sp, -8
@@ -29,8 +28,8 @@ main:
 	addiu	$sp, $sp, 8
 	
 	# Load saved registers, pop our stack frame and return
-	lw	$fp, 16($sp)
-	lw	$ra, 12($sp)
+	lw	$fp, 12($sp)
+	lw	$ra, 8($sp)
 	addiu	$sp, $sp, 16
 	jr	$ra
 
